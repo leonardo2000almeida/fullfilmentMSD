8914d0cf26553c2181a0c2ea3ae9b5086a9934ae
Leonardo Almeida <leonardo.oliveira@contazap.com>
2021-06-06 23:31:58 -0300

    fix initial commit

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 "use strict";
 
-import { WebhookClient } from "dialogflow-fulfillment-helper";
+import {WebhookProcessing} from './responses/intents.js';
 import express from "express";
-import {testFullfilmentIntentHandler } from "./responses/intents.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const webhookProcessing = (req, res) => {
-  const agent = new WebhookClient({ request: req, response: res });
-
-  let intentsMap = new Map();
-
-  intentsMap.set("fullfilment", testFullfilmentIntentHandler(agent));
-
-  agent.handleRequest(intentsMap);
-};
-
-app.post("/", function (req, res) {
-  webhookProcessing(req, res);
+app.post('/', function(req, res) {
+  WebhookProcessing(req, res);
 });
 
-app.get("/", (req, res) => {
-  return res.status(200).send("App on!");
+app.listen(process.env.PORT || 5000, function () {
+  console.info(`Application launched on port ${process.env.PORT || 5000}`);
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.info(`server is Running on port: ${process.env.PORT || 5000}`);
+app.get('/', function (req, res) {
+  return res.status(200).send('Application launched!');
 });
--- responses/intents.js
+++ responses/intents.js
@@ -1,5 +1,15 @@
-const testFullfilmentIntentHandler = (agent) => {
-  agent.add("pegou do heroku!");
+import { WebhookClient } from "dialogflow-fulfillment-helper";
+//never use the invocation of function 
+export function WebhookProcessing(req, res) {
+  const agent = new WebhookClient({ request: req, response: res });
+    
+  let intentsMap = new Map();
+    
+  intentsMap.set('teste', TesteIntentHandler);
+
+  agent.handleRequest(intentsMap);
 }
 
-export {testFullfilmentIntentHandler };
+function TesteIntentHandler(agent) {
+  agent.add('teste.');
+}
