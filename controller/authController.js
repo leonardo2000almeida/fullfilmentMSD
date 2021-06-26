10771f540b934680fd9bac74835814fafb112fc2
Leonardo Almeida <leonardo.oliveira@contazap.com>
2021-06-25 23:46:41 -0300

    change all letter in auth to lower case

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 const { sellers } = require("../services/autenticacao.json");
 
 const auth = async (email) => {
-  const result = sellers.find((seller) => seller.email === email);
+  const result = sellers.find((seller) => seller.email === email.toLowerCase());
   return result !== undefined
     ? { result, httpStatus: 200 }
     : { httpStatus: 404 };
